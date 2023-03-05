84430affb1871e53678b145c335974aed1d0863b
zzedddd <melegend.forever@protonmail.com>
2023-03-05 11:20:44 +0530

    tailwind conf

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICBASE_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 STATIC_ROOT = BASE_DIR / "djangoblog/static"
