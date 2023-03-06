1b9f2ffccdeff09c8ffb0ebf89ec8f5be99cebd1
zzedddd <melegend.forever@protonmail.com>
2023-03-07 01:08:23 +0530

    vercel conf

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5a6&*29$-9(l9n4lb&fyrcxf8qw%x=av+3+z$x4z--%y2g+9lh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'djangoblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
